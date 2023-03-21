0490342b2b44242394a1682c37ee70453d315871
Patryk <patryk.r.nowak@gmail.com>
2023-03-21 06:16:37 +0000

    fun with compile errors

--- section_4_getting_started/notes.cpp
+++ section_4_getting_started/notes.cpp
@@ -7,3 +7,37 @@
     - Errors
     - Warnings
 */
+
+/*
+    29. What are Compiler Errors?
+
+    Programming languages have rules
+
+    Syntax errors - something wrong with the structure
+    std::cout << "Errors << std::endl;
+
+    Semantic errors - something wrong with the meaning
+
+    a + b;
+
+    When it does not make sense to add a and b
+
+
+        .\compileErrors.cpp:5:18: warning: missing terminating " character
+            5 |     std::cout << "Hello world << std::endl;
+            |                  ^
+        .\compileErrors.cpp:5:18: error: missing terminating " character
+            5 |     std::cout << "Hello world << std::endl;
+            |                  ^~~~~~~~~~~~~~~~~~~~~~~~~~
+        .\compileErrors.cpp: In function 'int main()':
+        .\compileErrors.cpp:6:9: error: expected primary-expression before 'return'
+            6 |         return 0;
+
+-----------------
+
+        .\compileErrors.cpp: In function 'int main()':
+        .\compileErrors.cpp:5:40: error: 'endll' is not a member of 'std'; did you mean 'endl'?
+            5 |     std::cout << "Hello world" << std::endll;
+            |                                        ^~~~~
+            |                                        endl
+*/
