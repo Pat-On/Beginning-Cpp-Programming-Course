ab316633335002e623daa27fc1bf79951817ec46
Patryk <patryk.r.nowak@gmail.com>
2023-03-28 13:28:31 +0100

    section 7 arrays and vectors

---
+++ section_7_arrays_and_vectors/53_section_overview.cpp
@@ -0,0 +1,21 @@
+/*
+    53 section overview
+
+1st compound datatype
+
+- arrays and vectors
+    - arrays
+        - what they are
+        - why we use arrays
+        - declaration and initialization
+        - accessing array elements
+
+    - multi-dimensional arrays
+
+    (in modern c++ most of the times we are using vectors not an arrays)
+    - vectors
+        - what they are
+        - advantages vs. arrays
+        - declaration and initialization
+
+*/
