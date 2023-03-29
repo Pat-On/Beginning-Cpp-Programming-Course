23b13504011eb51ab4a9ec5273b97571adc43a44
Patryk <patryk.r.nowak@gmail.com>
2023-03-29 20:50:06 +0100

    just theory

---
+++ section_7_arrays_and_vectors/54_what_is_an_array.cpp
@@ -0,0 +1,26 @@
+/*
+
+    54 What is an array?
+
+- What is an array?
+    - compound data type od data structure
+        - collection of elements
+    - all elements are of the same type
+    - each element can be accessed directly
+
+Array Characteristics
+- fixed size
+- elements are all the same type
+- stored contiguously in memory
+- individual elements can be accessed by their position or index
+
+- first element is at index 0
+- last element is at index size - 1
+
+- no checking to see if you are out of bounds
+
+- always initialize arrays
+- very efficient
+- iteration (looping) is often used to process
+
+*/
