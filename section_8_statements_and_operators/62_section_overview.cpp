1748f00aa3654e85da4761890097e73b97efaba3
Patryk <patryk.r.nowak@gmail.com>
2023-03-30 08:48:03 +0100

    intro section 8

---
+++ section_8_statements_and_operators/62_section_overview.cpp
@@ -0,0 +1,18 @@
+/*
+                                    Section Overview
+
+    Expressions, Statements and Operators
+
+    - Expressions
+    - Statements and block statements
+    - Operators
+        - Assignment
+        - Arithmetic
+        - Increment and Decrement
+        - Equality
+        - Relational
+        - Logical
+        - Compound Assignment
+        - Precedence
+
+*/
