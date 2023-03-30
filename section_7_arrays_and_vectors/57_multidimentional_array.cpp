cd211b123d6502d21adbaec7682f9877ec520c2d
Patryk <patryk.r.nowak@gmail.com>
2023-03-30 07:23:08 +0100

    multidimensional arrays{

---
+++ section_7_arrays_and_vectors/57_multidimentional_array.cpp
@@ -0,0 +1,38 @@
+/*
+        57 Multidimensional Arrays
+
+
+    Declaring multi-dimensional arrays
+
+
+    element_type array_name [dim1_size][dim2_size]
+
+        int movie_rating [3][4];
+
+
+    Multi-dimensional arrays
+
+        const int rows {3};
+        const int cols{4};l
+
+        int movie_rating [rows][cols];
+
+
+    Accessing array elements in multi-dimensional arrays
+
+        cin >> movie_rating [1][2];
+        cout << movie_rating [1][2];
+
+
+    Initializing multi-dimensional array
+
+        int movie_rating [3][4]
+        {
+            {0, 4, 3, 5},
+            {2, 3, 3, 5},
+            {1, 4, 4, 5}
+        }
+
+
+
+*/
