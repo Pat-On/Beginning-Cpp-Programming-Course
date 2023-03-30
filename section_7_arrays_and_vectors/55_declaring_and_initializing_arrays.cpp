c66e50079922ceb439c699420ba804c7181ccd96
Patryk <patryk.r.nowak@gmail.com>
2023-03-30 06:11:14 +0100

    declaring and init arrays

---
+++ section_7_arrays_and_vectors/55_declaring_and_initializing_arrays.cpp
@@ -0,0 +1,34 @@
+/*
+    Arrays Declaring
+
+            Element_Type array_name [constant number of elements];
+
+
+            int test_scores [5];
+
+            int high_score_per_level [10];
+
+            const int days_in_year {365};
+
+            double hi_temperature [days_in_year];
+
+
+--------------------------------------------------------------------------------------------------
+
+    Arrays Initialization
+
+            Element_Type array_name [number of elements] {init list}
+
+
+            int test_scores [5] {100, 95, 99, 87, 88};
+
+            int high_score_per_level [10] {3,5}; // init to 3,5 and remaining to 0
+
+            const double days_in_year {365};
+            double hi_temperatures [days_in_year] {0} // init all to zero
+
+            int another_array [] {1, 2, 3, 4, 5} // size automatically calculated
+
+
+
+*/
