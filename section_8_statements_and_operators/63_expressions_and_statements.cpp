3b5154f00285859f1e9ec3d3e72e9fda2d6c874f
Patryk <patryk.r.nowak@gmail.com>
2023-03-30 20:47:07 +0100

    63

---
+++ section_8_statements_and_operators/63_expressions_and_statements.cpp
@@ -0,0 +1,51 @@
+/*
+
+    63 Expressions and Statements
+
+Expressions:
+
+an expression is:
+    - the most basic building block of program
+    - a sequence of operators and operands that specifies a computation
+    - computes a values from a number of operands
+    - there is much, much more to expressions - not necessary at this level
+
+
+    expressions - examples
+
+    34                          // literal
+    favorite_number             // variable
+    1.5 + 2.8                   // addition
+    2 * 5                       // multiplication
+    a > b                       // relational
+    a = b                       // assignment
+
+    ---------------------------
+
+    Statements
+    A statement is:
+        - a complete line of code that performs some action
+        - usually terminated with a semi-colon
+        - usually contain expressions
+        - C++ has many types of statements
+            - expressions
+            - null
+            - compound
+            - selection
+            - iteration
+            - declaration
+            - jump
+            - try block
+            - and others
+
+
+    Statements - examples:
+    int x;                  // declaration
+    favorite_number = 12;   // assignment
+    1.5 + 2.8;              // expression
+    x = 2 * 5               // assignment
+    if (a > b) cout << "a is greater than b";       // if
+
+
+
+*/
