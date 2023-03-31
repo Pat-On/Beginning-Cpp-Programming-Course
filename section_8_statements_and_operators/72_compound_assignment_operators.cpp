6c33524bdcaf44d3a9b2462d29f00ba7e1211307
Patryk <patryk.r.nowak@gmail.com>
2023-03-31 21:02:15 +0100

    72 compound operators

---
+++ section_8_statements_and_operators/72_compound_assignment_operators.cpp
@@ -0,0 +1,15 @@
+/*
+
+            72 Compound Assignment Operator
+
+    the same
+
+    +=      lhs += rhs      lhs = lhs + (rhs)
+    -=
+    *=
+    /=
+    %=
+
+    a *= b + c          a = a * (b + c)
+
+*/
