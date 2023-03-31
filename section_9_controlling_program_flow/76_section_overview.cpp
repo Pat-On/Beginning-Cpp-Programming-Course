29d94cb635b05ea2220a5f7a29adff201991fd55
Patryk <patryk.r.nowak@gmail.com>
2023-03-31 21:20:15 +0100

    start section 10

---
+++ section_9_controlling_program_flow/76_section_overview.cpp
@@ -0,0 +1,36 @@
+/*
+
+            Controlling Program Flow
+
+    - Sequence
+        - ordering statements sequentially
+
+    - Selection
+        - making decisions
+
+    - iteration
+        - looping or repeating
+
+
+        Selection - Decision Making
+            'if' statement
+            'if-else' statement
+            nested 'if'
+            'switch' statement
+            conditional operator '?:'
+
+        Iteration - Looping
+
+            for loop
+            range-based for loop
+            while loop
+            do-while loop
+            continue and break
+            infinite loops
+            nested loops
+
+            
+
+
+
+*/
