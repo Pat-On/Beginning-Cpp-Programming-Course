adecffb61e709e9270efe11c495e483be7aba919
Patryk <patryk.r.nowak@gmail.com>
2023-04-01 07:55:16 +0100

    conditional operator

---
+++ section_9_controlling_program_flow/81_conditional_operator.cpp
@@ -0,0 +1,18 @@
+/*
+
+                ?:
+
+
+        (cond_expr) ? expr1 : expr2
+
+- cond_expr evaluates to a boolean expression
+    - if cond_expr is true then the value of expr1 is returned
+    - if cond_expr is false then the value of expr2 is returned 
+
+- similar to if-else construct
+- ternary operator
+- very useful when used inline
+- very easy to abuse!
+
+
+*/
