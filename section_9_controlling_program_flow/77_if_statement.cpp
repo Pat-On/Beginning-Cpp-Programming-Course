5d456e95ae2189c30f39f4e26f71ea2598225f99
Patryk <patryk.r.nowak@gmail.com>
2023-04-01 07:33:25 +0100

    if and if-else

---
+++ section_9_controlling_program_flow/77_if_statement.cpp
@@ -0,0 +1,33 @@
+/*
+
+        if (expr)
+            statement
+    
+    - if the expression is true then execute the statement
+    - if the expression is false then skip the statement
+
+    example:
+
+    if (selection === 'A') 
+        cout << "You selected A";
+
+
+    ---------------------------------------
+
+    Block statement
+
+        {
+            //variable declarations
+            statement1;
+            statement2;
+            ...
+        }
+
+    - create a block of code by including more than one statement in code block {}
+    - blocks can also contain variable declarations
+    - these variables are visible only within the block - local scope
+
+
+
+
+*/
---
+++ section_9_controlling_program_flow/78_if_else_statement.cpp
@@ -0,0 +1,14 @@
+/*
+
+            if - else statement
+
+        if (expr)
+            statement;
+        else
+            statement 2
+
+
+    - if the expression is true then execute statement1
+    - if the expression is false then execute statement2
+
+*/
