a202821460314c10eeadbcd3cd828454d0d278f7
Patryk <patryk.r.nowak@gmail.com>
2023-04-01 08:08:05 +0100

    loops

---
+++ section_9_controlling_program_flow/82_looping.cpp
@@ -0,0 +1,49 @@
+/*
+
+        Looping
+
+    - the third basic building block of programming
+        - sequence
+        - selection
+        - iteration
+    - iteration or repetition
+    - allows the execution of a statement or block of statements repeatedly
+    - loops are mede up a loop condition and they body which contains the statements to repeat
+
+        ------------------------------------------------------------------
+
+    Some typical use-cases
+
+    execute a loop:
+        - a specific number of times
+        - for each element in a collection
+        - while a specific condition remains true
+        - until a specific condition becomes false
+        - until we reach the end of some input stream
+        - forever 
+        - many, many more 
+
+
+        ------------------------------------------------------------------
+
+    - for loop
+        - iterate a specific number of times
+
+    - range-based for loop
+        - one iteration for each element in a range or collection
+
+    - while loop
+        - iterate while a condition remains true
+        - stop when the condition becomes false
+        - check the condition at the beginning of every iteration
+
+    - do- while loop
+        - iterate while a condition remains true
+        - stop when the condition becomes false
+        - check the condition at the end of every iteration
+        
+    
+
+
+
+*/
