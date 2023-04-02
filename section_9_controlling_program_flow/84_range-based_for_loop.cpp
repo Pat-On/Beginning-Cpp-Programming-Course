a03ac7912778e1b3afe039ffe79a22044d7026a0
Patryk <patryk.r.nowak@gmail.com>
2023-04-02 06:46:37 +0100

    range-based + while loop

---
+++ section_9_controlling_program_flow/84_range-based_for_loop.cpp
@@ -0,0 +1,30 @@
+/*
+
+        #84 Range Based for Loop
+
+
+            introduced in C++ 11
+
+                for (var_type var_name: sequence)
+                    statement;   can use var_name
+
+                for (var_type var_name: sequence) {
+                    statements; // can use var_name
+                }
+
+    ---------------------------------------------------------
+        example:
+
+        int scores [] {100, 90, 97};
+
+        for (int score: scores)
+            cout << score << endl;;
+
+
+        auto type
+
+        for (auto score: scores)
+            cout << score << endl;
+
+
+*/
