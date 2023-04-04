cfe4bc662e2988d30a852433a82cfd899f5d4623
Patryk <patryk.r.nowak@gmail.com>
2023-04-04 06:09:17 +0100

    characxter functions

---
+++ section_10_characters_and_strings/94_character_functions.cpp
@@ -0,0 +1,35 @@
+/*
+
+        94 Character function
+
+
+
+    #include <cctype>
+
+    function_name(char)
+
+    - functions for testing characters
+    - functions for converting character case
+
+    Examples:
+
+
+        isalpha(c)              True if c is a letter
+        isalnum(c)              True if c is a letter or digfit
+        isdigit(c)              True if c is a digit
+        islower(c)              True if c is lowercase letter
+        isprint(c)              True if c is a printable character
+        ispunct(c)              True if c is a punctuation character
+        isupper(c)              True if c is an uppercase letter
+        isspace(c)              True if c is whitespace
+
+
+    Converting characters
+
+        tolower(c)      returns lowercase of c
+        toupper(c)      returns uppercase of c
+
+
+
+
+*/
