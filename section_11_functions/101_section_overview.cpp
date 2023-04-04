db268c309dfcba6023e440f32197fe99ba988420
Patryk <patryk.r.nowak@gmail.com>
2023-04-04 19:38:45 +0100

    section 11

---
+++ section_11_functions/101_section_overview.cpp
@@ -0,0 +1,16 @@
+/*
+
+        Function
+            - definition
+            - prototype
+            - Parameters and pass-by-value
+            - return statement
+            - default parameter values
+            - overloading
+            - passing arrays to function
+            - pass-by-reference
+            - inline functions
+            - auto return type
+            - recursive functions
+
+*/
