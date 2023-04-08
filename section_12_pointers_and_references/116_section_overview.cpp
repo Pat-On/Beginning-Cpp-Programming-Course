e89f8bda03854cd1256c638dd87e6d888625b5c6
Patryk <patryk.r.nowak@gmail.com>
2023-04-08 18:17:44 +0100

    Pointers!!

---
+++ section_12_pointers_and_references/116_section_overview.cpp
@@ -0,0 +1,24 @@
+/*
+
+                        Section Overview
+
+            - What is a pointer?
+            - declaring pointers
+            - storing addresses in pointers
+            - dereferencing pointers
+            - dynamic memory allocation
+            - pointer arithmetic
+            - pointers and arrays
+            - pass by reference with pointers
+            - const and pointers
+            - using pointers to functions
+            - potential pointer pitfall
+
+            - what is a reference?
+            - review passing references to functions
+            - const and references
+            - references variables in range-based for loops
+            - potential reference pitfall
+
+            - raw vs. smart pointers
+*/
