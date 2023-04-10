e9edcad216f942069054a8777c5ca257fdeef0db
Patryk <patryk.r.nowak@gmail.com>
2023-04-10 12:20:12 +0100

    overview

--- section_13_oop_classes_and_objects/134_section_overview.cpp
+++ section_13_oop_classes_and_objects/134_section_overview.cpp
@@ -2,6 +2,19 @@
 
             Section Overview
 
-    
+    - What is OOP
+    - What are classes and objects
+    - declaring classes and creating objects
+    - dot and pointer operators
+    - public and private access modifiers
+    - methods, constructors and destructors
+        - class methods
+        - default and overloaded constructors
+        - copy and move constructors
+        - shallow vs. deep copying
+        - this pointer
+    - static class members
+    - struct vs class
+    - friend of a class
 
 */
