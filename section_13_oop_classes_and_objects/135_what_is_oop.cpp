2bec174f2425d9db4501ae0994f664b13a658367
Patryk <patryk.r.nowak@gmail.com>
2023-04-10 14:16:57 +0100

    oop recap

---
+++ section_13_oop_classes_and_objects/135_what_is_oop.cpp
@@ -0,0 +1,87 @@
+/*
+    What is Object-Oriented Programming?
+
+    - Procedural Programming
+    - Procedural Programming limitations
+
+    - OO Programming concepts and their advantages
+    - OO Programming limitations
+
+    ---------------------------------
+
+    Procedural Programming
+    - Focus is on processes or actions that a program takes
+    - programs are typically a collection of functions
+    - data is declared separately
+    - data is passed as arguments into functions
+    - fairly easy to learn
+
+    Procedural Programming - Limitations
+
+    - Functions need to know the structure of the data
+        - if the structure of the data changes many functions must be changed
+
+    - as programs get larger they become more:
+        - difficult to understand
+        - difficult to maintain
+        - difficult to extend
+        - difficult to debug
+        - difficult to reuse code
+        - fragile and easier to break
+
+
+    ---------------------------------
+
+    What is OOP?
+
+    - Classes and Objects
+        - focus is on classes that model real-world domain entities
+        - allows developers to think at a higher level of abstraction
+        - used successfully in very large programs
+
+    - Encapsulation
+        - objects contain data AND operations that work on that data
+        - Abstract Data Type (ADT)
+
+    - Information-hiding
+        - implementation-specific logic can be hidden
+        - users of the class code to the interface since they do not need to know the implementation
+        - more abstraction
+        - easier to test, debug, maintain and extend
+
+    - Reusability
+        - easier to reuse classes in other applications
+        - faster development
+        - higher quality
+
+    - Inheritance
+        - can create new classes in term of existing classes
+        - reusability
+        - polymorphic classes
+
+    - Polymorphism and more...
+
+    ------------------------------------------------------------------------------------------------------------------------------------
+
+                LIMITATIONS
+
+    - Not a panacea
+        - OO Programming would not make bad code better
+        - not suitable for all types of problems
+        - not everything decomposes to class
+
+    - learning curve
+        - usually a steeper leaning curve, especially for C++
+        - many OO languages, many variation of OO concepts
+
+    - design
+        - usually more up-front design is necessary to create good models and hierarchies
+
+    - Programs can be
+        - larger in size
+        - slower
+        - more complex
+
+
+
+*/
---
+++ section_13_oop_classes_and_objects/136_what_are_classes_and_objects.cpp
@@ -0,0 +1,48 @@
+/*
+
+    What are Classes and Objects
+
+
+    - Classes
+        - blueprint from which objects are created
+        - a user-defined data-type
+        - has attributes (data)
+        - has methods (functions)
+        - can hide data and methods
+        - provides a public interface
+
+    - Example classes
+        - account
+        - employee
+        - image
+        - std::vector
+        - std::string
+
+
+------------------------------------------------------------------
+
+    - Objects
+        - created from a class
+        - represents a specific instance of a class
+        - can create many many objects
+        - each has its own identity
+        - each can use the defined class methods
+
+    - Example Account objects
+        - frank's account is an instance of  an account
+        - jim's account is an instance of an account
+        - each has its own balance, can make deposits, withdrawals, etc.
+
+
+------------------------------------------------------------------
+
+        int high_score;
+        int low_score;
+
+        Account frank_account;
+        Account jim_account;
+
+        std::vector<int> scores;
+        std::string name;
+
+*/
