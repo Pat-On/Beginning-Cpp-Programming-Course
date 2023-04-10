21f2b87b58608bb1dc816f54d12db6ffa4aa038c
Patryk <patryk.r.nowak@gmail.com>
2023-04-10 08:46:09 +0100

    Passing pointers to function

--- section_12_pointers_and_references/source/PassingPointers3/main.cpp
+++ section_12_pointers_and_references/source/PassingPointers3/main.cpp
@@ -7,32 +7,35 @@
 
 using namespace std;
 
-void display( const vector<string> *const  v) {
-//(*v).at(0) = "Funny";
-    for (auto str: *v)
+// function overloading
+void display(const vector<string> *const v)
+{
+    //(*v).at(0) = "Funny";
+    for (auto str : *v)
         cout << str << " ";
     cout << endl;
-  
- //   v = nullptr;
+
+    //   v = nullptr;
 }
 
-void display(int *array, int sentinel) {
+void display(int *array, int sentinel)
+{
     while (*array != sentinel)
         cout << *array++ << " ";
     cout << endl;
 }
 
-int main() {
+int main()
+{
 
-//    cout << "-----------------------------" << endl;
-//    vector<string> stooges {"Larry", "Moe", "Curly"};
-//    display(&stooges);
-   
+    //    cout << "-----------------------------" << endl;
+    //    vector<string> stooges {"Larry", "Moe", "Curly"};
+    //    display(&stooges);
+
     cout << "\n-----------------------------" << endl;
-    int scores[] {100,98,97,79,85,-1};
+    int scores[]{100, 98, 97, 79, 85, -1};
     display(scores, -1);
-    
-    cout  << endl;
+
+    cout << endl;
     return 0;
 }
-
