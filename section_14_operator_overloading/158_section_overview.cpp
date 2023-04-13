be7104793b68b599dc9216c669c9a0e1a9f13193
Patryk <patryk.r.nowak@gmail.com>
2023-04-13 21:22:40 +0100

    section 14 intro

---
+++ section_14_operator_overloading/158_section_overview.cpp
@@ -0,0 +1,14 @@
+/*
+
+                    Operator Overloading
+
+
+        - what is operator overloading?
+        - overloading the assignment operator (=)
+            -copy semantics
+            - move semantics
+        - overloading operators as member functions
+        - overloading operators as global functions
+        - overloading stream insertion (<<) and extraction operator (>>)
+
+*/
