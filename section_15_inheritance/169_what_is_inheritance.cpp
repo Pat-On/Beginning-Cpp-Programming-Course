366115c58944192056204f102447bc1ccd950853
Patryk <patryk.r.nowak@gmail.com>
2023-04-16 08:26:40 +0100

    recap

---
+++ section_15_inheritance/169_what_is_inheritance.cpp
@@ -0,0 +1,38 @@
+/*
+            What is inheritance
+
+    - What ios it and why is it used?
+        - provides a method for creating new classes from existing classes
+        - the new class contains the data and behaviors of the existing class
+        - allow for reuse of existing classes
+        - allows us to focus on the common attributes among a set of classes
+        - allows new classes to modify behaviors of existing classes to make it unique
+            - without actually modifying the original class
+
+
+    --------------------------------------------------
+
+    Related classes
+        - player, enemy, level boss, hero, super player etc
+        - account, savings account, checking account, trust account etc
+        - shape, line, oval, circle, square etc
+        - person, employee student, faculty, staff, administrator
+
+    --------------------------------------------------
+
+
+
+    Accounts
+
+    - account
+        - balance, deposit, withdraw
+    - saving account
+        - balance, deposit, withdraw, interest rate, ...
+    - checking account
+        - balance, deposit, withdraw, minimum balance, per check fee, ...
+    - trust Account
+        - balance, deposit, withdraw, interest rate, ...
+
+
+
+*/
