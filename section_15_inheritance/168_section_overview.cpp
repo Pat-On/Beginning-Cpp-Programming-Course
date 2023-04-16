d2b69692b580b50ea05f698af8ebbe8cb452c053
Patryk <patryk.r.nowak@gmail.com>
2023-04-16 08:19:29 +0100

    inheritance section - overview

---
+++ section_15_inheritance/168_section_overview.cpp
@@ -0,0 +1,24 @@
+/*
+
+                Section Overview
+
+        - What is inheritance?
+            - why is it useful?
+        
+        - Terminology and Notation
+        - Inheritance vs Composition
+        - Deriving classes from existing classes
+            - types of inheritance
+        - protected members and class access
+        - constructors and destructors
+            - passing arguments to base class constructors
+            - order of constructor and destructors calls
+        -  redefining base class methods
+        - class hierarchies
+        - multiple inheritance
+
+
+        
+
+
+*/
