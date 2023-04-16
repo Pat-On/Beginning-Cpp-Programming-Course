eda868cfde71b3e8289dbb220295280f88c24452
Patryk <patryk.r.nowak@gmail.com>
2023-04-16 08:34:27 +0100

    recap terminology

---
+++ section_15_inheritance/170_terminology_and_notation.cpp
@@ -0,0 +1,43 @@
+/*
+                Terminology and Notation
+
+        - inheritance
+            - process of creating a new class from existing classes
+            - reuse mechanism
+        - single inheritance
+            - a new class in created from another single class
+        - multiple inheritance 
+            -a new class is created from two (or more) other classes
+
+    ----------------------------------------------------------
+
+        Base class (parent class, super class)
+            - the class being extended or inherited from
+        
+        Derived class (child class, sub class)
+            - the class being created from the base class
+            - will inherit attributes and operation from base class
+
+
+    -----------------------------------------------------------
+
+        "Is-A" relationship
+            - public inheritance
+            - derived classes are sub-types of their base classes
+            - can use a derived class object wherever we use a base class object
+
+        - generalization
+            - combining similar classes into a single, more general class based on common attributes
+
+        - specialization
+            - creating new classes from existing classes proving more specialized attributes or operations
+        
+        - inheritance or class hierarchies 
+            - organization of our inheritance relationships
+
+            
+        
+
+
+
+*/
