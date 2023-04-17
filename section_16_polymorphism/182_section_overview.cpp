7ce89b75ecabd50601f62e7b53abbd5bcb47d5b8
Patryk <patryk.r.nowak@gmail.com>
2023-04-17 07:57:49 +0100

    polymorphism - overview

---
+++ section_16_polymorphism/182_section_overview.cpp
@@ -0,0 +1,12 @@
+/*
+                    Section Overview
+            - what is polymorphism?
+            - using base class pointers
+            - static vs dynamic binding
+            - virtual functions - functions bind on the run time
+            - virtual destructors
+            - the override and final specifiers
+            - using base class references
+            - pure virtual functions and abstract classes
+            - abstract classes as interfaces
+*/
