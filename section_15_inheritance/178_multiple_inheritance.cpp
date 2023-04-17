aa7c7903687ca8ec98e405e0eff3167bd0445266
Patryk <patryk.r.nowak@gmail.com>
2023-04-17 06:17:43 +0100

    multiple inheritance

---
+++ section_15_inheritance/178_multiple_inheritance.cpp
@@ -0,0 +1,38 @@
+/*
+
+                                    Multiple Inheritance
+
+
+                - A derived class inherits from two or more
+                    base classes at the same time
+
+                - The base classes may belong to unrelated class hierarchies
+
+
+                - a department chair
+                    - is-a faculty 
+                    - is-a administrator
+
+
+
+            C++ Syntax
+
+
+
+            class Department_Chair:
+                public Faculty, public Administrator {
+                    . . .
+            };
+
+
+            - Beyond the scope of this course
+            - some compelling use-cases
+            - easily misused
+            - can be very complex
+
+
+            
+
+
+
+*/
