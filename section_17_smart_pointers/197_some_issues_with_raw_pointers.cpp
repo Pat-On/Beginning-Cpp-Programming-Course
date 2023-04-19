1c8ffd6107a44c44b4ee580f19de34bcc9c6e827
Patryk <patryk.r.nowak@gmail.com>
2023-04-19 06:03:33 +0100

    issues with pointers

---
+++ section_17_smart_pointers/197_some_issues_with_raw_pointers.cpp
@@ -0,0 +1,20 @@
+/*
+
+            Some issues with raw pointers
+
+    - C++ provides absolute flexibility with memory management
+        - allocation
+        - deallocation
+        - lifetime management
+    
+    - Some potentially serious problems
+        - Uninitialized (wild) pointers
+        - memory leaks
+        - dangling pointers
+        - not exception safe
+    
+    - ownership 
+        - who owns the pointer?
+        - when should a pointer be deleted 
+
+*/
