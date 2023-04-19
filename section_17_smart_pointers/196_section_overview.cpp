604c6a1f75abc45199e485afbd634da3cb15fa34
Patryk <patryk.r.nowak@gmail.com>
2023-04-19 05:58:29 +0100

    intro

---
+++ section_17_smart_pointers/196_section_overview.cpp
@@ -0,0 +1,11 @@
+/*
+    Smart Pointers
+    - issues with raw pointers
+    - what are smart pointers?
+    - concepts of ownership and RAII
+    - C++ Smart Pointers
+        - Unique pointers (unique_prt)
+        - Shared pointers (shared_ptr)
+        - Weak pointers (weak_ptr)
+    - Custom deleters
+*/
