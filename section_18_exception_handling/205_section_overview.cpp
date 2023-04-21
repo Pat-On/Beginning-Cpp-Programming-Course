4b1fe50f32d14ec85e71f7659eec591fa4b2e467
Patryk <patryk.r.nowak@gmail.com>
2023-04-21 16:32:00 +0100

    overview

---
+++ section_18_exception_handling/205_section_overview.cpp
@@ -0,0 +1,13 @@
+/*
+
+        Exception Handling
+
+        - what is an exception?
+        - what is exception handling?
+        - what do we throw and catch expression?
+        - how does it affect flow of control?
+        - defining our own exception classes
+        - the standard library exception hierarchy
+            - std::exception and what()
+
+*/
