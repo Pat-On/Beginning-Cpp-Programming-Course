e453e4f96d6f6bdd9b6e0d06eb45541eaf472b66
Patryk <patryk.r.nowak@gmail.com>
2023-04-23 21:05:08 +0100

    lambda overview

---
+++ section_21_lambda_expressions/265_section_overview.cpp
@@ -0,0 +1,18 @@
+/*
+
+        Section Overview
+
+
+    C++ Lambda expressions
+
+    what is a lambda expression?
+        - motivation
+        - review of function objects (functors)
+        - relation between lambdas and function objects
+    structure of a lambda expression
+    types of lambda expressions
+        - stateless lambda expression
+        - stateful lambda expression (capturing context)
+    lambdas and the stl
+
+*/
