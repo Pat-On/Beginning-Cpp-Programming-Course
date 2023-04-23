9c3b53b8a901a4be71b8fa398bad43826664b05a
Patryk <patryk.r.nowak@gmail.com>
2023-04-23 07:50:08 +0100

    I/O and streams - chapter overview

---
+++ section_19_I_O_and_streams/215_section_overview.cpp
@@ -0,0 +1,13 @@
+/*
+
+                Section Overview
+
+        I/0 Streams
+
+        - Streams and I/O
+        - Steam manipulators
+        - Reading and writing to a text file
+        - Using string streams
+
+
+*/
