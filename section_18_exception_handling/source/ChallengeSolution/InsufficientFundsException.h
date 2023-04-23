2ecba03c32d233543a17e7df2eff164a32c1aad1
Patryk <patryk.r.nowak@gmail.com>
2023-04-23 07:47:00 +0100

    challenge

--- section_18_exception_handling/source/ChallengeSolution/InsufficientFundsException.h
+++ section_18_exception_handling/source/ChallengeSolution/InsufficientFundsException.h
@@ -1,11 +1,13 @@
 #ifndef __INSUFFICIENT_FUNDS_EXCEPTION_H__
 #define __INSUFFICIENT_FUNDS_EXCEPTION_H__
 
-class InsufficientFundsException : public std::exception {
+class InsufficientFundsException : public std::exception
+{
 public:
     InsufficientFundsException() noexcept = default;
     ~InsufficientFundsException() = default;
-    virtual const char *what() const noexcept {
+    virtual const char *what() const noexcept
+    {
         return "Insufficient funds exception";
     }
 };
--- section_18_exception_handling/source/ChallengeSolution/main.cpp
+++ section_18_exception_handling/source/ChallengeSolution/main.cpp
@@ -8,10 +8,12 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
     std::unique_ptr<Account> moes_account;
     std::unique_ptr<Account> larrys_account;
-    try {
+    try
+    {
         larrys_account = std::make_unique<Savings_Account>("Larry", -2000.0);
         std::cout << *larrys_account << std::endl;
     }
@@ -20,23 +22,27 @@ int main() {
         std::cerr << ex.what() << std::endl;
     }
 
-    try {
+    try
+    {
         moes_account = std::make_unique<Savings_Account>("Moe", 1000.0);
         std::cout << *moes_account << std::endl;
         moes_account->withdraw(500.0);
         std::cout << *moes_account << std::endl;
         moes_account->withdraw(1000.0);
         std::cout << *moes_account << std::endl;
-        
+        moes_account->withdraw(1000.0);
+        std::cout << *moes_account << std::endl;
+        moes_account->withdraw(1000.0);
+        std::cout << *moes_account << std::endl;
     }
     catch (const IllegalBalanceException &ex)
     {
         std::cerr << ex.what() << std::endl;
     }
-    catch (const InsufficientFundsException &ex) {
+    catch (const InsufficientFundsException &ex)
+    {
         std::cerr << ex.what() << std::endl;
     }
     std::cout << "Program completed successfully" << std::endl;
     return 0;
 }
-
