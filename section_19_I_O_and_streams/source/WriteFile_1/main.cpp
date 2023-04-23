4e19ad0ab47a467fc792559466cc9d06727e9c1b
Patryk <patryk.r.nowak@gmail.com>
2023-04-23 20:10:46 +0100

    live demo

--- section_19_I_O_and_streams/source/WriteFile_1/main.cpp
+++ section_19_I_O_and_streams/source/WriteFile_1/main.cpp
@@ -4,19 +4,30 @@
 #include <fstream>
 #include <string>
 
-int main()  {
-    std::ofstream out_file {"../output.txt", std::ios::app};
-    if (!out_file) {
+int main()
+{
+    std::ofstream out_file{"../output.txt"};                // default - truncate
+    std::ofstream out_file{"../output.txt", std::ios::app}; // append mode
+    // file exist or file create
+    if (!out_file)
+    {
         std::cerr << "Error creating file" << std::endl;
         return 1;
     }
+
+    // variable - container
     std::string line;
+
+    // get input from user
     std::cout << "Enter something to write to the file: ";
+    // console get line
     std::getline(std::cin, line);
+
+    // insert
     out_file << line << std::endl;
-    
+
+    // close file
     out_file.close();
-   
+
     return 0;
 }
-
