09dbbae380fd96649919d4782b993e8c582fc47f
Patryk <patryk.r.nowak@gmail.com>
2023-04-23 21:02:28 +0100

    overview STL

---
+++ section_20_the_standard_template_library_stl/237_section_overview.cpp
@@ -0,0 +1,24 @@
+/*
+
+            Section Overview
+
+    What is the STL
+    Generic Programming / Meta Programming
+        - preprocessor macros
+        - function templates
+        - class templates
+    STL container
+    STL Iterators
+    STL Algorithms
+
+    Array
+    Vector
+    Deque
+    List and Forward List
+    Set and Multi Set
+    Map and Multi Map
+    Stack and Queue
+    Priority Queue
+    Algorithms
+
+*/
