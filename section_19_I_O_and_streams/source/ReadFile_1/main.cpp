b9e3d518eaa52172ad953e8687a11ea13dad90ef
Patryk <patryk.r.nowak@gmail.com>
2023-04-23 18:12:53 +0100

    demo 1

--- section_19_I_O_and_streams/source/ReadFile_1/main.cpp
+++ section_19_I_O_and_streams/source/ReadFile_1/main.cpp
@@ -4,22 +4,24 @@
 #include <iostream>
 #include <fstream>
 
-int main() {
+int main()
+{
     std::ifstream in_file;
     std::string line;
     int num;
     double total;
-    
-    in_file.open("../test.txt");
-    if (!in_file) {
+
+    // in_file.open("../test.txt");
+    in_file.open("test.txt"); // win
+    if (!in_file)
+    {
         std::cerr << "Problem opening file" << std::endl;
         return 1;
     }
-    in_file >> line >> num >> total;
+    in_file >> line >> num >> total; // read. white space = stop read white space = stop
     std::cout << line << std::endl;
     std::cout << num << std::endl;
     std::cout << total << std::endl;
     in_file.close();
     return 0;
 }
-
--- section_19_I_O_and_streams/source/ReadFile_2/main.cpp
+++ section_19_I_O_and_streams/source/ReadFile_2/main.cpp
@@ -5,24 +5,29 @@
 #include <iomanip>
 #include <fstream>
 
-int main() {
+int main()
+{
     std::ifstream in_file;
     std::string line;
     int num;
     double total;
-    
-    in_file.open("../test.txt");
-    if (!in_file) {
+
+    in_file.open("test.txt");
+    if (!in_file)
+    {
         std::cerr << "Problem opening file" << std::endl;
         return 1;
     }
-    while (in_file >> line >> num >> total) {
-        std::cout << std::setw(10) << std::left << line 
-                       << std::setw(10) << num 
-                       << std::setw(10)  << total
-                       << std::endl;
+
+    // reading line by line - stream
+    // if anything is going to happen with this it would stop
+    while (in_file >> line >> num >> total)
+    {
+        std::cout << std::setw(10) << std::left << line
+                  << std::setw(10) << num
+                  << std::setw(10) << total
+                  << std::endl;
     }
     in_file.close();
     return 0;
 }
-
