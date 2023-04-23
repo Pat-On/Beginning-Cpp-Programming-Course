cff4cb26bf5a67215e3f7e4019bcd37e7ff4c3a7
Patryk <patryk.r.nowak@gmail.com>
2023-04-23 20:42:31 +0100

    enumeration - overview

---
+++ section_23_bonus_section_enumerations/288_section_overview.cpp
@@ -0,0 +1,19 @@
+/*
+
+                Section Overview
+
+
+        C++ Enumerations
+
+        What is an enumeration?
+            - Motivation
+        Structure of an enumeration
+        Types of enumerations
+            - unscoped enumeration
+            - scoped enumeration
+        Enumerations in use
+
+        
+
+
+*/
