d5535029018f12c5d96dc8f840fef9f3a47d4c5f
Patryk <patryk.r.nowak@gmail.com>
2023-04-29 09:33:02 +0100

    queue ide

--- section_20_the_standard_template_library_stl/source/Queue/main.cpp
+++ section_20_the_standard_template_library_stl/source/Queue/main.cpp
@@ -3,15 +3,17 @@
 #include <iostream>
 #include <queue>
 
-// This function displays a queue of 
+// This function displays a queue of
 // by repeatedly removing elements from the front
 // Note theat the queue is passed in by value so we
 // don't affect the passed in queue.
 
 template <typename T>
-void display(std::queue<T> q) {
+void display(std::queue<T> q)
+{
     std::cout << "[ ";
-    while (!q.empty()) {
+    while (!q.empty())
+    {
         T elem = q.front();
         q.pop();
         std::cout << elem << " ";
@@ -19,10 +21,11 @@ void display(std::queue<T> q) {
     std::cout << "]" << std::endl;
 }
 
-int main() {
+int main()
+{
     std::queue<int> q;
-    
-    for (int i: {1,2,3,4,5}) 
+
+    for (int i : {1, 2, 3, 4, 5})
         q.push(i);
     display(q);
     std::cout << "Front: " << q.front() << std::endl;
@@ -34,28 +37,27 @@ int main() {
     q.pop();
     q.pop();
     display(q);
-    
+
     while (!q.empty())
         q.pop();
     display(q);
 
     std::cout << "Size: " << q.size() << std::endl;
-    
+
     q.push(10);
     q.push(100);
     q.push(1000);
     display(q);
-    
+
     std::cout << "Front: " << q.front() << std::endl;
     std::cout << "Back:  " << q.back() << std::endl;
-    
+
     q.front() = 5;
     q.back() = 5000;
-    
+
     display(q);
     std::cout << "Front: " << q.front() << std::endl;
     std::cout << "Back:  " << q.back() << std::endl;
-    
+
     return 0;
 }
-
